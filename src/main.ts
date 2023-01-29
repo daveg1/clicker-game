69a12f0498be84719153498368fe48641bf613eb
David Graham <d.graham8@rgu.ac.uk>
2023-01-29 01:31:57 +0000

    add first zone; add random enemy pool; add assets for new blocks

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import './style.css'
 
 const game = {
 	money: 0,
-	enemy: {
+	stage: 1,
+	zone: 0,
+	currentEnemy: {
 		hp: 200,
 		meta_hp: 200,
 	},
@@ -23,10 +25,46 @@ const game = {
 	],
 }
 
+const enemyPool = [
+	[
+		{
+			name: 'Dirt',
+			hp: 200,
+			asset: 'dirt.png',
+		},
+		{
+			name: 'Sand',
+			hp: 200,
+			asset: 'sand.png',
+		},
+		{
+			name: 'Gravel',
+			hp: 200,
+			asset: 'gravel.png',
+		},
+	],
+]
+
 const blockElem = document.querySelector('#block') as HTMLDivElement
+const blockNameElem = document.querySelector('#block-name') as HTMLDivElement
 const moneyElem = document.querySelector('#money') as HTMLDivElement
-const hpElem = document.querySelector('#hp') as HTMLDivElement
+const hpElem = document.querySelector('#block-hp') as HTMLDivElement
 const upgradeElems = document.querySelectorAll('.upgrade') as NodeListOf<HTMLDivElement>
+
+function spawnEnemy() {
+	// Get random enemy from pool based on current stage
+	// todo: spawn enemy based on weight/chance (e.g. 10% chance for ore to spawn)
+	const pool = enemyPool[game.zone]
+	console.log(enemyPool, game.zone)
+	const enemy = pool[Math.floor(Math.random() * pool.length)]
+
+	game.currentEnemy.hp = enemy.hp
+	game.currentEnemy.meta_hp = enemy.hp
+
+	console.log(enemy.name, blockNameElem)
+	blockNameElem.textContent = enemy.name
+	blockElem.style.backgroundImage = `url(${enemy.asset})`
+}
 
 // Game loop
 function update() {
@@ -39,22 +77,22 @@ function update() {
 	}, 0)
 
 	// Apply damage to enemy health
-	game.enemy.hp -= curr_dps
+	game.currentEnemy.hp -= curr_dps
 
 	// Check enemy
-	if (game.enemy.hp <= 0) {
+	if (game.currentEnemy.hp <= 0) {
 		game.money += 100
 		// todo: spawnEnemy() method
-		game.enemy.hp = game.enemy.meta_hp
+		spawnEnemy()
 	}
 
 	moneyElem.textContent = `$${game.money}`
-	hpElem.style.width = `${(game.enemy.hp / game.enemy.meta_hp) * 100}%`
+	hpElem.style.width = `${(game.currentEnemy.hp / game.currentEnemy.meta_hp) * 100}%`
 }
 
 window.onload = () => {
 	setClickHandlers(blockElem, () => {
-		game.enemy.hp -= 10
+		game.currentEnemy.hp -= 10
 	})
 
 	upgradeElems.forEach((upgradeElem, index) => {
@@ -78,5 +116,6 @@ window.onload = () => {
 		})
 	})
 
+	spawnEnemy()
 	update()
 }
