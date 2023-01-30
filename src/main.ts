ab792209f1715aa2daa3934ffc519c444413be24
David Graham <d.graham8@rgu.ac.uk>
2023-01-30 00:56:14 +0000

    move upgrade-related functionality to upgrades.ts module

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
 import { game } from './globals/game'
 import { updateBlockHp, spawnBlock, initBlock } from './modules/block'
-import { setClickHandlers } from './modules/setClickerHandlers'
 import { updateStatusBar } from './modules/status-bar'
+import { initUpgrades, updateUpgrades } from './modules/upgrades'
 import './style.css'
 
 // todo: store list of zones
-
-// Upgrades
-const upgradeElems = document.querySelectorAll('.upgrade') as NodeListOf<HTMLDivElement>
 
 // Game loop
 function update() {
@@ -37,41 +34,17 @@ function update() {
 		spawnBlock()
 	}
 
-	upgradeElems.forEach((upgradeElem, index) => {
-		const button = upgradeElem.querySelector('.upgrade__button') as HTMLButtonElement
-		const upgrade = game.upgrades[index]
-
-		button.disabled = upgrade.cost > game.money
-	})
-
 	updateStatusBar()
 	updateBlockHp()
+	updateUpgrades()
 }
 
 window.onload = () => {
+	// Initialise game
 	initBlock()
+	initUpgrades()
 
-	upgradeElems.forEach((upgradeElem, index) => {
-		const button = upgradeElem.querySelector('.upgrade__button') as HTMLButtonElement
-		const level = upgradeElem.querySelector('.upgrade__level') as HTMLDivElement
-		const cost = upgradeElem.querySelector('.upgrade__cost') as HTMLDivElement
-		const upgrade = game.upgrades[index]
-
-		level.textContent = upgrade.level.toString()
-		cost.textContent = upgrade.cost.toString()
-
-		setClickHandlers(button, () => {
-			if (game.money >= upgrade.cost) {
-				game.money -= upgrade.cost
-				upgrade.level++
-				upgrade.cost += Math.floor(upgrade.cost * 0.15)
-
-				level.textContent = upgrade.level.toString()
-				cost.textContent = upgrade.cost.toString()
-			}
-		})
-	})
-
+	// Spawn first enemy and begin render loop
 	spawnBlock()
 	update()
 }
---
+++ src/modules/upgrades.ts
@@ -0,0 +1,36 @@
+import { game } from '../globals/game'
+import { setClickHandlers } from './setClickerHandlers'
+
+const upgradeElems = document.querySelectorAll('.upgrade') as NodeListOf<HTMLDivElement>
+
+export function initUpgrades() {
+	upgradeElems.forEach((upgradeElem, index) => {
+		const button = upgradeElem.querySelector('.upgrade__button') as HTMLButtonElement
+		const level = upgradeElem.querySelector('.upgrade__level') as HTMLDivElement
+		const cost = upgradeElem.querySelector('.upgrade__cost') as HTMLDivElement
+		const upgrade = game.upgrades[index]
+
+		level.textContent = upgrade.level.toString()
+		cost.textContent = upgrade.cost.toString()
+
+		setClickHandlers(button, () => {
+			if (game.money >= upgrade.cost) {
+				game.money -= upgrade.cost
+				upgrade.level++
+				upgrade.cost += Math.floor(upgrade.cost * 0.15)
+
+				level.textContent = upgrade.level.toString()
+				cost.textContent = upgrade.cost.toString()
+			}
+		})
+	})
+}
+
+export function updateUpgrades() {
+	upgradeElems.forEach((upgradeElem, index) => {
+		const button = upgradeElem.querySelector('.upgrade__button') as HTMLButtonElement
+		const upgrade = game.upgrades[index]
+
+		button.disabled = upgrade.cost > game.money
+	})
+}
